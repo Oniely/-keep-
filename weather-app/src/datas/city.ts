a4d17b0ee2b922e8c3db9f37f32800e09b57deb0
Oniely <jabagatniel09@gmail.com>
2023-12-13 10:18:15 +0800

    q

--- weather-app/src/datas/city.ts
+++
@@ -1,3 +0,0 @@
-import city from './city.json';
-
-export const capitalCity = city.map(item => item.city);
--- weather-app/src/datas/worldCities.ts
+++
@@ -1,3 +0,0 @@
-import worldCities from './world-cities.json';
-
-export const worldCity = worldCities.map(item => item.name);
